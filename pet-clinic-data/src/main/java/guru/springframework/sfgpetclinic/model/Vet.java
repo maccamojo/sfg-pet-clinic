f335ebd18a6048e2bc190cf1a7dd9764746fa520
matt.mcfarlane <matt.mcfarlane@NECNSWNB31372.NECAU.NEC.COM.AU>
2020-05-05 22:36:02 +1000

    Added Pets and Contact Info to Owners on startup. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-	private Set<Speciality> specialities;
+	private Set<Speciality> specialities = new HashSet<>();   // initialise here to avoid a NullPointerException in T=the DataLoader (Best Practice)
 
 	public Set<Speciality> getSpecialities() {
 		return specialities;

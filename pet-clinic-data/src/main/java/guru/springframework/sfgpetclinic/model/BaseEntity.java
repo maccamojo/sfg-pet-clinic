48afa854e0828b44e86450d3d1df626d5da61513
matt.mcfarlane <matt.mcfarlane@NECNSWNB31372.NECAU.NEC.COM.AU>
2021-03-23 18:13:06 +1100

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,6 +2,11 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
  * 
@@ -9,8 +14,11 @@ import java.io.Serializable;
  *
  * @author Matt McFarlane
  */
+@MappedSuperclass    // telling JPA that we don't need this specific class mapped to the database
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;  // Hibernate recommendation is to use 'box' types, rather than primitives. Box types can have the value 'null', but primitives cannot.
 
 	public Long getId() {

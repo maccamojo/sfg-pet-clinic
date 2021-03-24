087940c9e4d8616d5a52c614e5c9f99388745ed8
matt.mcfarlane <matt.mcfarlane@NECNSWNB31372.NECAU.NEC.COM.AU>
2021-03-24 16:06:54 +1100

    Updated Vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,8 +3,20 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+	@ManyToMany(fetch = FetchType.EAGER)     // JPA is going to try to load the Specialties upfront (all at once)
+	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), 
+		inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Speciality> specialities = new HashSet<>();   // initialise here to avoid a NullPointerException in the DataLoader (Best Practice)
 
 	public Set<Speciality> getSpecialities() {
